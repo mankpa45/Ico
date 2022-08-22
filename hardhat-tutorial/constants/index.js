b60a8f4aff9fa21dd21efcd9d3cbd94afdba7c3a
mankpa45 <78081343+mankpa45@users.noreply.github.com>
2022-08-22 11:34:22 +0100

    new file:   hardhat-tutorial/.gitignore 	new file:   hardhat-tutorial/README.md 	new file:   hardhat-tutorial/constants/index.js 	new file:   hardhat-tutorial/contracts/CryptoDevToken.sol 	new file:   hardhat-tutorial/contracts/ICryptoDevs.sol 	new file:   hardhat-tutorial/hardhat.config.js 	new file:   hardhat-tutorial/package-lock.json 	new file:   hardhat-tutorial/package.json 	new file:   hardhat-tutorial/scripts/deploy.js 	new file:   hardhat-tutorial/test/Lock.js 	new file:   my-app/.eslintrc.json 	new file:   my-app/.gitignore 	new file:   my-app/README.md 	new file:   my-app/constants/index.js 	new file:   my-app/next.config.js 	new file:   my-app/package-lock.json 	new file:   my-app/package.json 	new file:   my-app/pages/_app.js 	new file:   my-app/pages/api/hello.js 	new file:   my-app/pages/index.js 	new file:   my-app/public/CyptoDevs/0.svg 	new file:   my-app/public/favicon.ico 	new file:   my-app/public/vercel.svg     new file:   my-app/styles/Home.module.css 	new file:   my-app/styles/globals.css

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xf2a474166B52B119C655602B94204C56e57e03ca";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
